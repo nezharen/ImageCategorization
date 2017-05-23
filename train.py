5ea40f2f53d1aace2e4c4b22b7a60f1d5b3f4f32
nezharen <nezharen1995@gmail.com>
2017-05-23 17:18:39 +0800

    fucking variable init

--- train.py
+++ train.py
@@ -4,14 +4,13 @@ import tensorflow as tf
 def read_image_label_list_file_and_convert_image_to_tfrecord(directory, filename):
     global sess
     global converted_to_tfrecord
-    global saver
 
     f = open(os.path.join(directory, filename), 'r')
     image_filenames = []
     image_labels = []
     for line in f:
         image_id, image_label = line[:-2].split(' ')
-        if not sess.run(converted_to_tfrecord):
+        if not converted_to_tfrecord:
             image_content = tf.read_file(os.path.join(directory, image_id + '.jpg'))
             image = tf.image.decode_jpeg(image_content)
             resized_image = tf.cast(tf.image.resize_images(image, [250, 250]), tf.uint8)
@@ -25,31 +24,19 @@ def read_image_label_list_file_and_convert_image_to_tfrecord(directory, filename
             writer.close()
             print('generated ' + image_id + '.tfrecord')
         image_filenames.append(os.path.join(directory, image_id + '.tfrecord'))
-    if not sess.run(converted_to_tfrecord):
-        sess.run(converted_to_tfrecord.assign(True))
-        saver.save(sess, 'train.ckpt')
     return image_filenames
 
-sess = tf.Session()
-
-converted_to_tfrecord = tf.Variable(False, name = 'converted_to_tfrecord')
-saver = tf.train.Saver()
 
 #restore from checkpoint or init variables
+sess = tf.Session()
+converted_to_tfrecord = False
 ckpt = tf.train.get_checkpoint_state(os.path.dirname(__file__))
 if ckpt and ckpt.model_checkpoint_path:
-    saver.restore(sess, ckpt.model_checkpoint_path)
-    print('restored from %s' % ckpt.model_checkpoint_path)
-else:
-    init_variables = tf.global_variables_initializer()
-    sess.run(init_variables)
+    converted_to_tfrecord = True
 
+#convert image to tfrecord and read
 images_filename = read_image_label_list_file_and_convert_image_to_tfrecord(os.path.join('ic-data', 'train'), 'train.label')
 filename_queue = tf.train.string_input_producer(images_filename)
-
-coord = tf.train.Coordinator()
-threads = tf.train.start_queue_runners(sess = sess, coord = coord)
-
 reader = tf.TFRecordReader()
 _, serialized_example = reader.read(filename_queue)
 features = tf.parse_single_example(
@@ -59,8 +46,29 @@ features = tf.parse_single_example(
         'label': tf.FixedLenFeature([], tf.int64),
     })
 image_raw = tf.decode_raw(features['image'], tf.uint8)
-image = tf.reshape(image_raw, [250, 250, 3])
+image = tf.image.rgb_to_grayscale(tf.reshape(image_raw, [250, 250, 3]))
+image = tf.cast(image, tf.float32) * (1. / 255)
 label = tf.cast(features['label'], tf.int32)
+min_after_dequeue = 500
+batch_size = 3
+capacity = min_after_dequeue + 3 * batch_size
+image_batch, label_batch = tf.train.shuffle_batch(
+    [image, label], batch_size = batch_size, capacity = capacity,
+    min_after_dequeue = min_after_dequeue)
+conv2d_layer_one = tf.contrib.layers.convolution2d(
+    image_batch,
+    num_outputs = 32,
+    kernel_size = (5, 5),
+    activation_fn = tf.nn.relu,
+    stride = (2, 2),
+    trainable = True)
+
+coord = tf.train.Coordinator()
+threads = tf.train.start_queue_runners(sess = sess, coord = coord)
+sess.run(tf.global_variables_initializer())
+saver = tf.train.Saver()
+saver.save(sess, 'train.ckpt')
+print sess.run(conv2d_layer_one)
 
 coord.request_stop()
 coord.join(threads)
